032e035a201b29137647bf5c6c3f3a5072a3665b
Nikita-Babich <nikita.babich.a@gmail.com>
2023-05-13 13:57:49 +0300

    +typedef

---
+++ svd.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define nl printf("\n")
+#define ELEM(mat,i,j) mat[i * (mat->cols) + j]
+
+typedef struct{
+	unsigned int rows;
+	unsigned int cols;
+	float*
+}MAT;
