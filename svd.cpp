869a7bd0f5b2961fa6f4a5409433c070195975dc
Nikita-Babich <nikita.babich.a@gmail.com>
2023-05-13 14:56:40 +0300

    matrix allocation

--- svd.cpp
+++ svd.cpp
@@ -10,5 +10,46 @@
 typedef struct{
 	unsigned int rows;
 	unsigned int cols;
-	float*
+	float* elem;
 }MAT;
+
+MAT* mat_create_with_type(unsigned int rows, unsigned int cols){
+	//Can ptr still be NULL after typecasting?
+	MAT* ptr = (MAT*)malloc( sizeof(unsigned int)*2 + sizeof(float)*rows*cols );
+	if(ptr == NULL){
+		free(ptr);
+		return NULL;
+	}
+	ptr->rows = rows;
+	ptr->cols = cols;
+	return ptr;
+}
+
+//MAT* mat_create_by_file(char* filename){
+//	
+//}
+
+//char mat_save(MAT* mat, char* filename){
+//	
+//}
+
+//void mat_destroy(MAT* mat){
+//	
+//}
+
+//void mat_unit(MAT* mat){
+//	
+//}
+
+//void mat_random(MAT* mat){
+//	
+//}
+
+//void mat_print(MAT* mat){
+//	
+//}
+
+int main(){
+	
+}
+
